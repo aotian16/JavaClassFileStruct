ff3d117cfc3d13be484a20d1850a6a3162adb679
tongjin <aotian16@gmail.com>
2016-11-13 16:17:19 +0800

    init

---
+++ src/JavaClassFileStruct/src/main/java/com/qefee/jcfs/App.java
@@ -0,0 +1,10 @@
+package com.qefee.jcfs;
+
+/**
+ * Created by tongjin on 2016/11/13.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
